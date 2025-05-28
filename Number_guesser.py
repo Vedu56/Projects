7fe17bdfaca568adfe0612acd77f09347e281bd2
VT <118688668+Vedu56@users.noreply.github.com>
2025-05-28 17:22:31 +0100

    Add files via upload

---
+++ Number_guesser.py
@@ -0,0 +1,65 @@
+import random
+
+lower_bound = 1
+upper_bound = 100
+max_attempts = 7
+
+random_number = random.randint(lower_bound, upper_bound)
+
+def user_guess():
+
+    while True:
+
+        try:
+            user_number = int(input("Enter a number between 1 and 100: "))
+
+            if lower_bound <= user_number <= upper_bound:
+                return user_number
+            
+            else:
+                print("Error! Please enter a number between 1 and 100!")
+
+        except ValueError:
+            print("Error! Please enter a valid number!")
+
+def check_guess(user_number, random_number):
+
+    if user_number == random_number:
+        return "Correct number guessed!"
+        
+    elif user_number < random_number:
+        return "Too low!"
+
+    elif user_number > random_number:
+        return "Too high!"
+    
+def play_game():
+
+    attempts = 0
+    won = False
+
+    while attempts < max_attempts:
+
+        attempts += 1
+        user_input = user_guess()
+        result = check_guess(user_input, random_number)
+
+        if result == "Correct":
+            print(f"Congratulations! You have guessed the number {random_number} correctly!")
+            won = True
+            break
+
+        else:
+            print(f"{result} is not correct! Try again!")
+
+    if not won:
+        print(f"Sorry! You have ran out of attempts! The number was {random_number}!")
+
+if __name__ == "__main__":
+    print("Welcome to the number guessing game!")
+    play_game()
+
+
+        
+
+       
